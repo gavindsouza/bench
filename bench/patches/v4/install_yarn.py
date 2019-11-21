b3536a7471100bcacda12b7b5067873416696332
Gavin D'souza <gavin18d@gmail.com>
2019-11-21 19:49:20 +0530

    fix: change yarn install method

--- bench/patches/v4/install_yarn.py
+++ bench/patches/v4/install_yarn.py
@@ -2,4 +2,4 @@ import os
 from bench.utils import exec_cmd
 
 def execute(bench_path):
-	exec_cmd('npm install yarn', os.path.join(bench_path, 'apps/frappe'))
+	exec_cmd('curl --compressed -o- -L https://yarnpkg.com/install.sh | bash', os.path.join(bench_path, 'apps/frappe'))
